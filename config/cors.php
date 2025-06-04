29f75b7c02f268c0bc75dd8bac10c8fb7abc5f1b
elmatas04 <amatasrabasco@gmail.com>
2025-06-04 11:13:55 +0200

    Guardando cambios locales antes del pull

--- config/cors.php
+++ config/cors.php
@@ -1,3 +1,4 @@
+HEAD
 <?php
 
 return [
@@ -19,3 +20,25 @@ return [
     'supports_credentials' => false
 
 ];
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:5173'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => null,
+
+    'supports_credentials' => false
+
+];
+ 4c78075 (Guardando cambios locales antes del pull)
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
+HEAD
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,14 @@ Route::post('/solicitud', function (Request $request) {
         'datos' => $request->all()
     ]);
 });
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/solicitud', function (Request $request) {
+    return response()->json([
+        'mensaje' => 'Solicitud recibida correctamente',
+        'datos' => $request->all()
+    ]);
+});
+4c78075 (Guardando cambios locales antes del pull)
