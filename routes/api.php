4142179158baf617cc26094ae903f3455e88530e
elmatas24 <121408582+elmatas24@users.noreply.github.com>
2025-06-05 13:12:14 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,3 @@
-HEAD
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,14 +7,3 @@ Route::post('/solicitud', function (Request $request) {
         'datos' => $request->all()
     ]);
 });
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/solicitud', function (Request $request) {
-    return response()->json([
-        'mensaje' => 'Solicitud recibida correctamente',
-        'datos' => $request->all()
-    ]);
-});
-4c78075 (Guardando cambios locales antes del pull)
