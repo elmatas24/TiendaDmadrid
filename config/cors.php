4f7fdb681fff05ee9b072d7d954fa5ba45b2349a
elmatas24 <121408582+elmatas24@users.noreply.github.com>
2025-06-05 11:31:34 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -20,25 +20,4 @@ return [
     'supports_credentials' => false
 
 ];
-<?php
 
-return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => null,
-
-    'supports_credentials' => false
-
-];
- 4c78075 (Guardando cambios locales antes del pull)
