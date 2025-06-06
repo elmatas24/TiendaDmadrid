6d4a761818e209ba225cd6492d306f05c54f91aa
elmatas24 <121408582+elmatas24@users.noreply.github.com>
2025-06-06 13:48:27 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -7,8 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'https://*.vercel.app',
+       '^https:\/\/.*\.vercel\.app$',
     ],
 
     'allowed_origins_patterns' => [],
